c9fe13f7c7d4862f25fb7741d6da55258cf56746
Andre Thomas <andrethomas6@yahoo.com>
2020-10-17 12:44:22 -0700

    Added react-reveal and added animation to product list.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Zoom";
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => {
-            return (
-              <li key={product._id}>
-                <div className="product">
-                  <a href={"#" + product._id}>
-                    <img src={product.images} alt={product.title}></img>
-                    <p>{product.title}</p>
-                  </a>
-                  <div className="product-price">
-                    <div>{"$" + product.price}</div>
-                    <button onClick={() => this.props.addToCart(product)} className="button primary">Add to Cart</button>
+        <Fade bottom cascade>
+          <ul className="products">
+            {this.props.products.map((product) => {
+              return (
+                <li key={product._id}>
+                  <div className="product">
+                    <a href={"#" + product._id}>
+                      <img src={product.images} alt={product.title}></img>
+                      <p>{product.title}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{"$" + product.price}</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        Add to Cart
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </Fade>
       </div>
     );
   }
